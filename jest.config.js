2e8fb3f1b13f9294c84a3fd5db2040be78d3489a
Teun De Clercq - GITHUB <teundeclercq@hotmail.com>
2025-11-27 16:12:21 +0100

    adding global timeout for tests

--- jest.config.js
+++ jest.config.js
@@ -1,6 +1,7 @@
 module.exports = {
     testEnvironment: "node",
     testMatch: ["**/tests/**/*.test.js"],
+    testTimeout: 30000,
     verbose: true,
     forceExit: true,
     detectOpenHandles: true,
