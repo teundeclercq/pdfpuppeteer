2a27019688971efb4bce467a9e7f4aeb2a1cfac4
Teun de Clercq <teundeclercq@hotmail.com>
2025-11-27 15:10:35 +0100

    adjusted routes

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 require('dotenv').config();
 
 const express = require('express');
-const pdfRouter = require('routes/pdf')
+const pdfRouter = require('./routes/pdf');
 
 const app = express();
 const PORT = process.env.PORT || 3000;
